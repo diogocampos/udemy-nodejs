8e266ec6d499607b99b8415b322ea53970024488
Diogo Campos <diogocampos@users.noreply.github.com>
2018-08-02 13:15:09 -0300

    Add Socket.IO

--- chat-app/index.js
+++ chat-app/index.js
@@ -1,6 +1,20 @@
+const http = require('http')
+const socketio = require('socket.io')
+
 const app = require('./server/app')
 
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', socket => {
+  console.log('Connection')
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected')
+  })
+})
+
 const port = process.env.PORT || 3000
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
