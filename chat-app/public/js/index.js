71d55e46a7f617ffb71e63a6d1ad487cfbf27d4f
Diogo Campos <diogocampos@users.noreply.github.com>
2018-08-03 10:50:51 -0300

    Add message form and list of messages

--- chat-app/public/js/index.js
+++ chat-app/public/js/index.js
@@ -4,16 +4,34 @@ socket.on('connect', () => {
   console.log('Connected to server')
 })
 
-socket.on('newMessage', message => {
-  console.log(
-    `${new Date(message.createdAt)} - ${message.from}: ${message.text}`
-  )
-})
-
 socket.on('disconnect', () => {
   console.log('Disconnected from server')
 })
 
-socket.emit('createMessage', { from: 'Frank', text: 'Hi' }, ack => {
-  console.log('Ack:', ack)
+// receiving messages
+
+const $messageList = $('#message-list')
+
+socket.on('newMessage', message => {
+  $messageList.append(
+    $('<li></li>').text(
+      `${new Date(message.createdAt)} - ${message.from}: ${message.text}`
+    )
+  )
+})
+
+// sending messages
+
+const $messageInput = $('input[name=message]')
+
+$('#message-form').on('submit', e => {
+  e.preventDefault()
+  socket.emit(
+    'createMessage',
+    { from: 'User', text: $messageInput.val() },
+    ack => {
+      $messageInput.val('')
+      console.log('Ack:', ack)
+    }
+  )
 })
